203d994a531bbcd73d8cb12f7923b48defe4a557
agustinperalta <43344932+agustinperalta@users.noreply.github.com>
2020-05-23 22:34:57 -0300

    Updates

---
+++ precache-manifest.6863c63482374ac7fef8b4def1463df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecaf4c2150b68b7e14cc2268263c5e3",
+    "url": "/personajes-marvel-buscador/index.html"
+  },
+  {
+    "revision": "664b0f7f4fa3ce0e63b0",
+    "url": "/personajes-marvel-buscador/static/css/main.5e092bb1.chunk.css"
+  },
+  {
+    "revision": "079dc3b5c891818250dc",
+    "url": "/personajes-marvel-buscador/static/js/2.30088690.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/personajes-marvel-buscador/static/js/2.30088690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664b0f7f4fa3ce0e63b0",
+    "url": "/personajes-marvel-buscador/static/js/main.535a531b.chunk.js"
+  },
+  {
+    "revision": "b8a55ab1db85b48918e3",
+    "url": "/personajes-marvel-buscador/static/js/runtime-main.118f8837.js"
+  }
+]);
